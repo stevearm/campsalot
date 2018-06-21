c1d411a199b9ea4474b4e175923a58005000fbea
Steve Armstrong <steve@horsefire.com>
2018-06-20 22:34:59 -0700

    Can fill out form and check if results are available or not

--- bin/reserve-california.js
+++ bin/reserve-california.js
@@ -6,11 +6,9 @@ program
   .parse(process.argv);
 
 campground = "Henry Cowell Redwoods SP";
-startDate = "07/13/2018";
+startDate = new Date("09/13/2018");
 length = 2;
 
-console.log("Pre");
 reserveCalifornia.getSites(campground, startDate, length).then((sites) => {
-    console.log("Got sites %s", sites);
+    console.log("Got sites", sites);
 });
-console.log("Post");
