08d7ab1ed9f75e6384c36dc88f78cef7a020c1aa
Steve Armstrong <steve@horsefire.com>
2018-06-21 21:03:55 -0700

    Adding debug logging

--- bin/reserve-california.js
+++ bin/reserve-california.js
@@ -1,5 +1,4 @@
 const program = require('commander');
-const reserveCalifornia = require("../lib/reserve-california");
 
 program
   .option('-v, --verbose', 'Make more verbose')
@@ -9,6 +8,11 @@ campground = "Henry Cowell Redwoods SP";
 startDate = new Date("09/13/2018");
 length = 2;
 
-reserveCalifornia.getSites(campground, startDate, length).then((sites) => {
+if (program.verbose) {
+  // Same thing is accomplished with `DEBUG=campsalot* node bin/reserve-california.js`
+  require("debug").enable("campsalot*");
+}
+
+require("../lib/reserve-california").getSites(campground, startDate, length).then((sites) => {
     console.log("Got sites", sites);
 });
